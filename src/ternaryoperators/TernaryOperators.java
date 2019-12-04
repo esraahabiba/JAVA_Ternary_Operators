6400d4a6ed9dc55ed41f42fe107fe806415e5733
esraahabiba <esraahabiba2018@gmail.com>
2019-12-04 18:32:26 +0200

    first commit

---
+++ src/ternaryoperators/TernaryOperators.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ternaryoperators;
+
+/**
+ *
+ * @author gt
+ */
+public class TernaryOperators {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        int x = 5 ; 
+        int y = 10 ; 
+        int z = x < y ? 5 : 10 ; // condition ? true : false
+        System.out.println(" Z Equal = "+z); // codition is ture so print 5
+
+    }
+    
+}
